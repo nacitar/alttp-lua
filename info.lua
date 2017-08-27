14a510e6b01698f9be0b40ed1d2d437ca4a195a8
Jacob McIntosh <nacitar@ubercpp.com>
2017-08-26 22:08:13 -0500

    Now draws a freaking SNES controller!

--- info.lua
+++ info.lua
@@ -181,24 +181,19 @@ function ScaledDrawer:draw_cross(x, y, border_color,
   drawer:draw_rect(5+x, 3+y, 2, 2, right_color)
   drawer:draw_rect(3+x, 5+y, 2, 2, bottom_color)
 end
-function ScaledDrawer:draw_buttons(x, y, border_color,
+function ScaledDrawer:draw_button(x, y, border_color, color)
+  drawer:draw_rect(0+x, 1+y, 4, 2, border_color)
+  drawer:draw_rect(1+x, 0+y, 2, 4, border_color)
+  drawer:draw_rect(1+x, 1+y, 2, 2, color)
+end
+function ScaledDrawer:draw_simple_buttons(x, y, border_color,
     left_color, top_color, right_color, bottom_color)
 
-  drawer:draw_rect(0+x, 4+y, 4, 2, border_color)
-  drawer:draw_rect(1+x, 3+y, 2, 4, border_color)
-  drawer:draw_rect(1+x, 4+y, 2, 2, left_color)
-
-  drawer:draw_rect(4+x, 0+y, 2, 4, border_color)
-  drawer:draw_rect(3+x, 1+y, 4, 2, border_color)
-  drawer:draw_rect(4+x, 1+y, 2, 2, top_color)
-
-  drawer:draw_rect(6+x, 4+y, 4, 2, border_color)
-  drawer:draw_rect(7+x, 3+y, 2, 4, border_color)
-  drawer:draw_rect(7+x, 4+y, 2, 2, right_color)
-
-  drawer:draw_rect(4+x, 6+y, 2, 4, border_color)
-  drawer:draw_rect(3+x, 7+y, 4, 2, border_color)
-  drawer:draw_rect(4+x, 7+y, 2, 2, bottom_color)
+  -- TODO: simplify using draw_button()
+  drawer:draw_button(0+x, 3+y, border_color, left_color)
+  drawer:draw_button(3+x, 0+y, border_color, top_color)
+  drawer:draw_button(6+x, 3+y, border_color, right_color)
+  drawer:draw_button(3+x, 6+y, border_color, bottom_color)
 end
 function ScaledDrawer:draw_slanted_button(x, y, border_color, color)
   drawer:draw_rect(0+x, 2+y, 2, 2, border_color)
@@ -263,8 +258,178 @@ function draw_input_hud(x, y, pressed)
   top_color = pressed['X'] and COLOR_X or COLOR_RELEASED
   right_color = pressed['A'] and COLOR_A or COLOR_RELEASED
   bottom_color = pressed['B'] and COLOR_B or COLOR_RELEASED
-  drawer:draw_buttons(19, 0, 'black',
+  drawer:draw_simple_buttons(19, 0, 'black',
       left_color, top_color, right_color, bottom_color)
+end
+
+function draw_snes_controller(x, y, pressed)
+  local COLOR_BORDER = RGBA(0x00, 0x00, 0x00, 0xFF)
+  local COLOR_FACE = RGBA(0xE1, 0xE1, 0xE1, 0xFF)
+  local COLOR_DARK_FACE = RGBA(0x48, 0x48, 0x48, 0xFF)
+  local COLOR_Y_PRESSED = RGBA(0x7C, 0xD2, 0x44, 0xFF)
+  local COLOR_X_PRESSED = RGBA(0x24, 0xBA, 0xFC, 0xFF)
+  local COLOR_A_PRESSED = RGBA(0xFC, 0x02, 0x04, 0xFF)
+  local COLOR_B_PRESSED = RGBA(0xFC, 0xFE, 0x04, 0xFF)
+  local COLOR_RELEASED = COLOR_DARK_FACE
+  local COLOR_Y_RELEASED = COLOR_RELEASED
+  local COLOR_X_RELEASED = COLOR_RELEASED
+  local COLOR_A_RELEASED = COLOR_RELEASED
+  local COLOR_B_RELEASED = COLOR_RELEASED
+  local COLOR_L_RELEASED = COLOR_RELEASED
+  local COLOR_R_RELEASED = COLOR_RELEASED
+  local COLOR_SELECT_RELEASED = COLOR_RELEASED
+  local COLOR_START_RELEASED = COLOR_RELEASED
+  local COLOR_ARROW_RELEASED = COLOR_RELEASED
+
+  local COLOR_L_PRESSED = COLOR_B_PRESSED
+  local COLOR_R_PRESSED = COLOR_B_PRESSED
+  local COLOR_SELECT_PRESSED = COLOR_B_PRESSED
+  local COLOR_START_PRESSED = COLOR_B_PRESSED
+  local COLOR_ARROW_PRESSED = COLOR_B_PRESSED
+
+  drawer = ScaledDrawer(x, y, 1)
+  ---- MAIN CONTROLLER
+  -- face (expects border to overlap it, to do this with fewer rectangles)
+  drawer:draw_rect(8, 3, 29, 1, COLOR_FACE)  -- top row of face
+  drawer:draw_rect(5, 4, 35, 15, COLOR_FACE)  -- main-section of face
+  drawer:draw_rect(6, 19, 8, 2, COLOR_FACE)  -- left-lower face
+  drawer:draw_rect(31, 19, 8, 2, COLOR_FACE)  -- right-lower face
+  drawer:draw_rect(3, 5, 2, 14, COLOR_FACE) -- left of dpad face
+  drawer:draw_rect(40, 5, 2, 14, COLOR_FACE) -- right of button face
+  drawer:draw_rect(1, 7, 2, 10, COLOR_FACE) -- left edge face
+  drawer:draw_rect(42, 7, 2, 10, COLOR_FACE) -- right edge face
+
+  -- border
+  drawer:draw_rect(8, 2, 29, 1, COLOR_BORDER)  -- top border
+  drawer:draw_rect(6, 3, 2, 1, COLOR_BORDER)  -- next 2 left
+  drawer:draw_rect(37, 3, 2, 1, COLOR_BORDER)  --next 2 right
+  drawer:draw_rect(4, 4, 2, 1, COLOR_BORDER)  -- next 2 left
+  drawer:draw_rect(39, 4, 2, 1, COLOR_BORDER)  --next 2 right
+  drawer:draw_rect(3, 5, 1, 1, COLOR_BORDER)  -- next 1 left
+  drawer:draw_rect(41, 5, 1, 1, COLOR_BORDER)  --next 1 right
+  drawer:draw_rect(2, 6, 1, 1, COLOR_BORDER)  -- next 1 left
+  drawer:draw_rect(42, 6, 1, 1, COLOR_BORDER)  --next 1 right
+  drawer:draw_rect(1, 7, 1, 2, COLOR_BORDER)  -- next 2 left
+  drawer:draw_rect(43, 7, 1, 2, COLOR_BORDER)  --next 2 right
+  drawer:draw_rect(0, 9, 1, 6, COLOR_BORDER)  -- next 6 left
+  drawer:draw_rect(44, 9, 1, 6, COLOR_BORDER)  --next 6 right
+  drawer:draw_rect(1, 15, 1, 2, COLOR_BORDER)  -- next 2 left
+  drawer:draw_rect(43, 15, 1, 2, COLOR_BORDER)  --next 2 right
+  drawer:draw_rect(2, 17, 1, 1, COLOR_BORDER)  -- next 1 left
+  drawer:draw_rect(42, 17, 1, 1, COLOR_BORDER)  --next 1 right
+  drawer:draw_rect(3, 18, 1, 1, COLOR_BORDER)  -- next 1 left
+  drawer:draw_rect(41, 18, 1, 1, COLOR_BORDER)  --next 1 right
+  -- going down the curved lower ends
+  drawer:draw_rect(4, 19, 2, 1, COLOR_BORDER)  -- next 2 left-left
+  drawer:draw_rect(14, 19, 2, 1, COLOR_BORDER)  -- next 2 left-right
+  drawer:draw_rect(29, 19, 2, 1, COLOR_BORDER)  -- next 2 right-left
+  drawer:draw_rect(39, 19, 2, 1, COLOR_BORDER)  -- next 2 right-right
+  drawer:draw_rect(6, 20, 2, 1, COLOR_BORDER)  -- next 2 left-left
+  drawer:draw_rect(12, 20, 2, 1, COLOR_BORDER)  -- next 2 left-right
+  drawer:draw_rect(31, 20, 2, 1, COLOR_BORDER)  -- next 2 right-left
+  drawer:draw_rect(37, 20, 2, 1, COLOR_BORDER)  -- next 2 right-right
+  -- final edges
+  drawer:draw_rect(16, 18, 13, 1, COLOR_BORDER)  -- mid-lower border
+  drawer:draw_rect(8, 21, 4, 1, COLOR_BORDER)  -- left-lower border
+  drawer:draw_rect(33, 21, 4, 1, COLOR_BORDER)  -- right-lower border
+  -- dpad circle
+  drawer:draw_rect(8, 6, 4, 1, COLOR_DARK_FACE)  -- N
+  drawer:draw_rect(12, 7, 2, 1, COLOR_DARK_FACE)  -- N NE
+  drawer:draw_rect(14, 8, 1, 2, COLOR_DARK_FACE)  -- E NE
+  drawer:draw_rect(15, 10, 1, 4, COLOR_DARK_FACE)  -- E
+  drawer:draw_rect(14, 14, 1, 2, COLOR_DARK_FACE)  -- E SE
+  drawer:draw_rect(12, 16, 2, 1, COLOR_DARK_FACE)  -- S SE
+  drawer:draw_rect(8, 17, 4, 1, COLOR_DARK_FACE)  -- S
+  drawer:draw_rect(6, 16, 2, 1, COLOR_DARK_FACE)  -- S SW
+  drawer:draw_rect(5, 14, 1, 2, COLOR_DARK_FACE)  -- W SW
+  drawer:draw_rect(4, 10, 1, 4, COLOR_DARK_FACE)  -- W
+  drawer:draw_rect(5, 8, 1, 2, COLOR_DARK_FACE)  -- W NW
+  drawer:draw_rect(6, 7, 2, 1, COLOR_DARK_FACE)  -- N NW
+
+  ---- button circle
+  -- TOP-RIGHT
+  drawer:draw_rect(33, 4, 4, 1, COLOR_DARK_FACE)  -- N
+  drawer:draw_rect(36, 5, 3, 1, COLOR_DARK_FACE)  -- next row down
+  drawer:draw_rect(37, 6, 3, 1, COLOR_DARK_FACE)  -- next row down
+  drawer:draw_rect(38, 7, 2, 2, COLOR_DARK_FACE)  -- middle NE
+  drawer:draw_rect(40, 7, 1, 3, COLOR_DARK_FACE)  -- next col right
+  drawer:draw_rect(41, 8, 1, 3, COLOR_DARK_FACE)  -- next col right
+  -- RIGHT-BOTTOM
+  drawer:draw_rect(42, 10, 1, 4, COLOR_DARK_FACE)  -- E
+  drawer:draw_rect(41, 13, 1, 3, COLOR_DARK_FACE)  -- next col left
+  drawer:draw_rect(40, 14, 1, 3, COLOR_DARK_FACE)  -- next col left
+  drawer:draw_rect(39, 15, 1, 3, COLOR_DARK_FACE)  -- next col left
+  drawer:draw_rect(38, 16, 1, 3, COLOR_DARK_FACE)  -- next col left
+  drawer:draw_rect(37, 17, 1, 2, COLOR_DARK_FACE)  -- next col left
+  drawer:draw_rect(36, 18, 1, 1, COLOR_DARK_FACE)  -- next pixel left
+  -- BOTTOM-LEFT
+  drawer:draw_rect(33, 19, 4, 1, COLOR_DARK_FACE)  -- S
+  drawer:draw_rect(31, 18, 3, 1, COLOR_DARK_FACE)  -- next row up
+  drawer:draw_rect(30, 17, 3, 1, COLOR_DARK_FACE)  -- next row up
+  drawer:draw_rect(30, 15, 2, 2, COLOR_DARK_FACE)  -- middle SW
+  drawer:draw_rect(29, 14, 1, 3, COLOR_DARK_FACE)  -- next col left
+  drawer:draw_rect(28, 13, 1, 3, COLOR_DARK_FACE)  -- next col left
+  -- LEFT-TOP
+  drawer:draw_rect(27, 10, 1, 4, COLOR_DARK_FACE)  -- W
+  drawer:draw_rect(28, 8, 1, 3, COLOR_DARK_FACE)  -- next col right
+  drawer:draw_rect(29, 7, 1, 3, COLOR_DARK_FACE)  -- next col right
+  drawer:draw_rect(30, 6, 1, 3, COLOR_DARK_FACE)  -- next col right
+  drawer:draw_rect(31, 5, 1, 3, COLOR_DARK_FACE)  -- next col right
+  drawer:draw_rect(32, 5, 1, 2, COLOR_DARK_FACE)  -- next col right
+  drawer:draw_rect(33, 5, 1, 1, COLOR_DARK_FACE)  -- next pixel right
+  -- DIVIDER
+  drawer:draw_rect(32, 14, 1, 1, COLOR_DARK_FACE) -- bottom left
+  drawer:draw_rect(33, 13, 1, 1, COLOR_DARK_FACE)
+  drawer:draw_rect(34, 12, 1, 1, COLOR_DARK_FACE)
+  drawer:draw_rect(35, 11, 1, 1, COLOR_DARK_FACE)
+  drawer:draw_rect(36, 10, 1, 1, COLOR_DARK_FACE)
+  drawer:draw_rect(37, 9, 1, 1, COLOR_DARK_FACE) -- top right
+
+  ---- Arrows/Buttons
+  left_color = pressed['LEFT'] and COLOR_ARROW_PRESSED or COLOR_ARROW_RELEASED
+  top_color = pressed['UP'] and COLOR_ARROW_PRESSED or COLOR_ARROW_RELEASED
+  right_color = pressed['RIGHT'] and COLOR_ARROW_PRESSED or COLOR_ARROW_RELEASED
+  bottom_color = pressed['DOWN'] and COLOR_ARROW_PRESSED or COLOR_ARROW_RELEASED
+  drawer:draw_cross(6, 8, COLOR_BORDER,
+      left_color, top_color, right_color, bottom_color)
+
+  drawer:draw_button(29, 10, COLOR_BORDER,
+      pressed['Y'] and COLOR_Y_PRESSED or COLOR_Y_RELEASED)
+  drawer:draw_button(33, 6, COLOR_BORDER,
+      pressed['X'] and COLOR_X_PRESSED or COLOR_X_RELEASED)
+  drawer:draw_button(37, 10, COLOR_BORDER,
+      pressed['A'] and COLOR_A_PRESSED or COLOR_A_RELEASED)
+  drawer:draw_button(33, 14, COLOR_BORDER,
+      pressed['B'] and COLOR_B_PRESSED or COLOR_B_RELEASED)
+
+
+  color = pressed['SELECT'] and COLOR_SELECT_PRESSED or COLOR_SELECT_RELEASED
+  drawer:draw_slanted_button(17, 12, COLOR_BORDER, color)
+  color = pressed['START'] and COLOR_START_PRESSED or COLOR_START_RELEASED
+  drawer:draw_slanted_button(22, 12, COLOR_BORDER, color)
+
+  ---- Shoulder Buttons
+  drawer:draw_rect(4, 3, 1, 1, COLOR_BORDER)  -- first col of left shoulder
+  drawer:draw_rect(5, 2, 1, 1, COLOR_BORDER)  -- next col
+  drawer:draw_rect(6, 1, 2, 1, COLOR_BORDER)  -- next 2 cols
+  drawer:draw_rect(8, 0, 7, 1, COLOR_BORDER)  -- next 7 cols
+  drawer:draw_rect(15, 1, 1, 1, COLOR_BORDER)  -- last col of left shoulder
+
+  drawer:draw_rect(29, 1, 1, 1, COLOR_BORDER)  -- first col of right shoulder
+  drawer:draw_rect(30, 0, 7, 1, COLOR_BORDER)  -- next 7 cols
+  drawer:draw_rect(37, 1, 2, 1, COLOR_BORDER)  -- next 2 cols
+  drawer:draw_rect(39, 2, 1, 1, COLOR_BORDER)  -- next col
+  drawer:draw_rect(40, 3, 1, 1, COLOR_BORDER)  -- last col of right shoulder
+
+  color = pressed['L'] and COLOR_L_PRESSED or COLOR_L_RELEASED
+  drawer:draw_rect(5, 3, 1, 1, color)
+  drawer:draw_rect(6, 2, 2, 1, color)
+  drawer:draw_rect(8, 1, 7, 1, color)
+
+  color = pressed['R'] and COLOR_R_PRESSED or COLOR_R_RELEASED
+  drawer:draw_rect(30, 1, 7, 1, color)
+  drawer:draw_rect(37, 2, 2, 1, color)
+  drawer:draw_rect(39, 3, 1, 1, color)
 end
 
 function snes9x_button_string(pressed)
@@ -301,4 +466,5 @@ return {
   snes9x_button_string = snes9x_button_string,
   buffered_buttons = buffered_buttons,
   draw_input_hud = draw_input_hud,
+  draw_snes_controller = draw_snes_controller,
 }
--- overlay.lua
+++ overlay.lua
@@ -6,12 +6,17 @@ local var = require(THIS_DIR .. 'var')
 
 local engine = emu or snes9x
 
+local HudStyle = {
+  MINIMAL = 0,
+  SNES = 1,
+}
 local HudLocation = {
   TOP = 0,
   BOTTOM = 1,
 }
 
 HUD_LOCATION = HudLocation.BOTTOM
+HUD_STYLE = HudStyle.SNES
 -- this mimics practice hack, good for select buffering
 HUD_MOVEMENT_FRAME_ONLY = true
 
@@ -28,20 +33,31 @@ while true do
       'Tempbunny: ' .. info.tempbunny_string(),
       'Buttons: ' .. info.snes9x_button_string(last_buttons),
   })
-  if HUD_LOCATION == HudLocation.TOP then
+  if HUD_STYLE == HudStyle.SNES then
+    hud_x = 106
+    if HUD_LOCATION == HudLocation.TOP then
+      hud_y = 3
+    else -- if HUD_LOCATION == HudLocation.BOTTOM then
+      hud_y = 199  -- 202 is lowest it can draw
+    end
+  else  -- if HUD_STYLE == HudStyle.MINIMAL then
     hud_x = 114
-    hud_y = 3  -- 1 is at edge, but looks better halfway to upper hud
-  else -- if HUD_LOCATION == HudLocation.BOTTOM then
-    hud_x = 114
-    -- 214 is the lowest it can draw
-    hud_y = 211
+    if HUD_LOCATION == HudLocation.TOP then
+      hud_y = 3  -- 1 is at edge, but looks better halfway to upper hud
+    else -- if HUD_LOCATION == HudLocation.BOTTOM then
+      hud_y = 211  -- 214 is lowest it can draw
+    end
   end
   if HUD_MOVEMENT_FRAME_ONLY then
     buttons = last_movement_frame_buttons
   else
     buttons = last_buttons
   end
-  info.draw_input_hud(hud_x, hud_y, buttons)
+  if HUD_STYLE == HudStyle.SNES then
+    info.draw_snes_controller(hud_x, hud_y, buttons)
+  else
+    info.draw_input_hud(hud_x, hud_y, buttons)
+  end
 
   last_buttons = info.buffered_buttons()
   mode = var.mode:read()
